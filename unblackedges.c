56a74133741ac3df014f1c4574638b4750728971
elizabethhom <59620870+elizabethhom@users.noreply.github.com>
2020-08-11 19:09:35 -0700

    Update unblackedges.c

--- unblackedges.c
+++ unblackedges.c
@@ -1,11 +1,11 @@
 /*
- *     COMP40 - SPRING 2020
- *     HW2 - iii
- *     ELIZABETH HOM & THEO NUNEZ
- *     EHOM01 & TNUNEZ01
- *     10 FEB 2020
+ * unblackedges.c
  * 
- *     unblackedges.c - Unblacks the edges of a supplied pbm.
+ * COMP40 - SPRING2020
+ * Elizabeth Hom (ehom01) & Theo Nunez (trnunez01)
+ * Last Modified: February 10, 2020
+ * 
+ * Purpose: Unblacks the edges of a supplied PBM image.
  */
 
 #include <stdio.h>
@@ -19,37 +19,40 @@
 #define BLACK 1
 #define WHITE 0
 
+/*
+ * Coordinate
+ *
+ * Describes a coordinate of an image bit by its column and row.
+ */
 typedef struct Coordinate {
     int col;
     int row;
 } Coordinate;
 
 Bit2_T pbmRead(FILE *inputfp);
-void pbmWrite(FILE *outputfp, Bit2_T bit2);
-void printValues(int col, int row, Bit2_T bit2, int b, void *p1);
-void unblackLeftEdge(Bit2_T bit2);
-void unblackTopEdge(Bit2_T bit2);
-void unblackRightEdge(Bit2_T bit2);
-void unblackBottomEdge(Bit2_T bit2);
-void unblackAllEdges(Bit2_T bit2);
-void findNeighbors(Bit2_T bit2, Stack_T stack, int col, int row);
+void   pbmWrite(FILE *outputfp, Bit2_T bit2);
+void   printValues(int col, int row, Bit2_T bit2, int b, void *p1);
+void   unblackLeftEdge(Bit2_T bit2);
+void   unblackTopEdge(Bit2_T bit2);
+void   unblackRightEdge(Bit2_T bit2);
+void   unblackBottomEdge(Bit2_T bit2);
+void   unblackAllEdges(Bit2_T bit2);
+void   findNeighbors(Bit2_T bit2, Stack_T stack, int col, int row);
 
 int main(int argc, char *argv[])
 {
     assert(argc <= 2);
-
+    
     if (argc == 1) {
         Bit2_T mainBit2 = pbmRead(stdin);
         assert(mainBit2 != NULL);
 
         unblackAllEdges(mainBit2);
         pbmWrite(stdout, mainBit2);
-
         Bit2_free(&mainBit2);
     } else if (argc == 2) {
         FILE *fp = fopen(argv[1], "rb");
         assert(fp != NULL);
-
         Bit2_T mainBit2 = pbmRead(fp);
         assert(mainBit2 != NULL);
 
@@ -61,17 +64,16 @@ int main(int argc, char *argv[])
     }
 }
 
-/* 
- * NAME:   pbmRead
- * DOES:   copies the bits from the pbm file into a Bit2_T named
- *         bit2
- * INPUT:  a file pointer from the pbm file
- * OUTPUT: returns the created bit2 containing the values from the pbm file
+/*
+ *  pbmRead()
+ *
+ *  Parameters: file pointer to PBM
+ *  Does:       Copies bits from PBM image into Bit2_T bit2
+ *  Returns:    Created bit2 populated with PBM file values
  */
 Bit2_T pbmRead(FILE *inputfp)
 {
     Pnmrdr_T reader = Pnmrdr_new(inputfp);
-
     Pnmrdr_mapdata data = Pnmrdr_data(reader);
 
     assert(data.type == Pnmrdr_bit);
@@ -80,27 +82,30 @@ Bit2_T pbmRead(FILE *inputfp)
 
     Bit2_T bit2 = Bit2_new(data.width, data.height);
 
+    // Copies PBM into bit2, bit by bit
     for (int i = 0; i < Bit2_height(bit2); i++) {
         for (int j = 0; j < Bit2_width(bit2); j++) {
             Bit2_put(bit2, j, i, Pnmrdr_get(reader));
         }
     }
-
     Pnmrdr_free(&reader);
 
     return bit2;
 }
 
-/* 
- * NAME:   findNeighbors
- * DOES:   pushes neighboring bits that are black to the stack
- * INPUT:  a bit2, a stack structure, the col (int) and row (int) of the bit
- *         whose neighbors are to be found. It is a checked runtime error if an
- *         instance of a Coordinate struct created is NULL.
- * OUTPUT: returns nothing
+/*
+ *  findNeighbors()
+ *
+ *  Parameters: Bit2_T, Stack, current bit's col and row
+ *              *** It is a checked runtime error if an instance of the
+ *              Coordinate created is NULL.
+ *  Does:       Pushes a bit's neighboring black bits to the Stack
+ *  Returns:    NA
  */
 void findNeighbors(Bit2_T bit2, Stack_T stack, int col, int row)
 {
+    /* Checks Northern bit. If black, populates Coordinate and pushes
+     * onto Stack */
     if (col - 1 >= 0 && Bit2_get(bit2, col - 1, row) == BLACK) {
         Coordinate *left = (Coordinate *)malloc(sizeof(Coordinate));
         assert(left != NULL);
@@ -111,6 +116,7 @@ void findNeighbors(Bit2_T bit2, Stack_T stack, int col, int row)
         Stack_push(stack, left);
     }
 
+    // Checks Southern bit
     if (col + 1 < Bit2_width(bit2) && Bit2_get(bit2, col + 1, row) == BLACK) {
         Coordinate *right = (Coordinate *)malloc(sizeof(Coordinate));
         assert(right != NULL);
@@ -120,6 +126,7 @@ void findNeighbors(Bit2_T bit2, Stack_T stack, int col, int row)
         Stack_push(stack, right);
     }
 
+    // Checks Western bit
     if (row - 1 >= 0 && Bit2_get(bit2, col, row - 1) == BLACK) {
         Coordinate *up = (Coordinate *)malloc(sizeof(Coordinate));
         assert(up != NULL);
@@ -129,6 +136,7 @@ void findNeighbors(Bit2_T bit2, Stack_T stack, int col, int row)
         Stack_push(stack, up);
     }
 
+    // Checks Eastern bit
     if (row + 1 < Bit2_height(bit2) && Bit2_get(bit2, col, row + 1) == BLACK) {
         Coordinate *down = (Coordinate *)malloc(sizeof(Coordinate));
         assert(down != NULL);
@@ -139,12 +147,13 @@ void findNeighbors(Bit2_T bit2, Stack_T stack, int col, int row)
     }
 }
 
-/* 
- * NAME:   unblackAllEdges
- * DOES:   drives the unblacking of the pbm file's edges starting from the
- *         top, bottom, left, to the right edge.
- * INPUT:  the bit2
- * OUTPUT: returns nothing
+/*
+ *  unblackAllEdges()
+ *
+ *  Parameters: Bit2_T
+ *  Does:       Drives unblacking of PBM image's edges, starting from the
+ *              top, bottom, left, to right edge
+ *  Returns:    NA
  */
 void unblackAllEdges(Bit2_T bit2)
 {
@@ -154,30 +163,30 @@ void unblackAllEdges(Bit2_T bit2)
     unblackRightEdge(bit2);
 }
 
-/* 
- * NAME:   unblackTopEdge
- * DOES:   unblacks the black bits on the top edge and all their neighbors by
- *         performing a depth first search mechanic, and frees stack memory
- * INPUT:  bit2
- * OUTPUT: returns nothing
+/*
+ *  unblackTopEdge()
+ *
+ *  Parameters: Bit2_T
+ *  Does:       Unblacks black bits along top edge and neighbors by performing
+ *              a DFS algorithm. Frees associated stack memory.
+ *  Returns:    NA
  */
 void unblackTopEdge(Bit2_T bit2)
 {
     int topEdgeIndex = 0;
     Stack_T neighbors = Stack_new();
+    
     for (int i = 0; i < Bit2_width(bit2); i++) {
-
-        /* if the current bit is black, change it to white and add its
-         * neighbors that are black to the stack
-         */
+        
+        /* If the current bit is black, change to white and add black neighbors
+         * to the stack */
         if (Bit2_get(bit2, i, topEdgeIndex) == BLACK) {
             Bit2_put(bit2, i, topEdgeIndex, WHITE);
             findNeighbors(bit2, neighbors, i, topEdgeIndex);
         }
 
-        /* while the stack is not empty, change each bit to white and add its
-         * neighbors that are black to the stack
-         */
+        /* While the stack is not empty, change each bit to white and add its
+         * black neighbors to the stack */
         while (Stack_empty(neighbors) != 1) {
             Coordinate *topElem = Stack_pop(neighbors);
             Bit2_put(bit2, topElem->col, topElem->row, WHITE);
@@ -190,18 +199,19 @@ void unblackTopEdge(Bit2_T bit2)
     Stack_free(&neighbors);
 }
 
-
-/* 
- * NAME:   unblackBottomEdge
- * DOES:   unblacks the black bits on the bottom edge and all their neighbors
- *         by performing a depth first search mechanic, and frees stack memory
- * INPUT:  bit2
- * OUTPUT: returns nothing
+/*
+ *  unblackBottomEdge()
+ *
+ *  Parameters: Bit2_T
+ *  Does:       Unblacks black bits along bottom edge and neighbors by performing
+ *              a DFS algorithm. Frees associated stack memory.
+ *  Returns:    NA
  */
 void unblackBottomEdge(Bit2_T bit2)
 {
     int bottomEdgeIndex = Bit2_height(bit2) - 1;
     Stack_T neighbors = Stack_new();
+    
     for (int i = 0; i < Bit2_width(bit2); i++) {
         if (Bit2_get(bit2, i, bottomEdgeIndex) == BLACK) {
             Bit2_put(bit2, i, bottomEdgeIndex, WHITE);
@@ -220,17 +230,19 @@ void unblackBottomEdge(Bit2_T bit2)
     Stack_free(&neighbors);
 }
 
-/* 
- * NAME:   iterateLeftEdge
- * DOES:   unblacks the black bits on the left edge and all their neighbors
- *         by performing a depth first search mechanic, and frees stack memory
- * INPUT:  bit2
- * OUTPUT: returns nothing
+/*
+ *  unblackLeftEdge()
+ *
+ *  Parameters: Bit2_T
+ *  Does:       Unblacks black bits along left edge and neighbors by performing
+ *              a DFS algorithm. Frees associated stack memory.
+ *  Returns:    NA
  */
 void unblackLeftEdge(Bit2_T bit2)
 {
     int leftEdgeIndex = 0;
     Stack_T neighbors = Stack_new();
+    
     for (int i = 0; i < Bit2_height(bit2); i++) {
         if (Bit2_get(bit2, leftEdgeIndex, i) == BLACK) {
             Bit2_put(bit2, leftEdgeIndex, i, WHITE);
@@ -249,17 +261,19 @@ void unblackLeftEdge(Bit2_T bit2)
     Stack_free(&neighbors);
 }
 
-/* 
- * NAME:   iterateRightEdge
- * DOES:   unblacks the black bits on the right edge and all their neighbors
- *         by performing a depth first search mechanic, and frees stack memory
- * INPUT:  bit2
- * OUTPUT: returns nothing
+/*
+ *  unblackRightEdge()
+ *
+ *  Parameters: Bit2_T
+ *  Does:       Unblacks black bits along right edge and neighbors by performing
+ *              a DFS algorithm. Frees associated stack memory.
+ *  Returns:    NA
  */
 void unblackRightEdge(Bit2_T bit2)
 {
     int rightEdgeIndex = Bit2_width(bit2) - 1;
     Stack_T neighbors = Stack_new();
+    
     for (int i = 0; i < Bit2_height(bit2); i++) {
         if (Bit2_get(bit2, rightEdgeIndex, i) == BLACK) {
             Bit2_put(bit2, rightEdgeIndex, i, WHITE);
@@ -278,11 +292,12 @@ void unblackRightEdge(Bit2_T bit2)
     Stack_free(&neighbors);
 }
 
-/* 
- * NAME:   pbmWrite
- * DOES:   writes the bit2 out to the given file pointer
- * INPUT:  a file pointer to be written out to and the bit2
- * OUTPUT: returns nothing
+/*
+ *  pbmWrite()
+ *
+ *  Parameters: output file pointer, Bit2_T
+ *  Does:       Writes processed Bit2_T to given file pointer
+ *  Returns:    NA
  */
 void pbmWrite(FILE *outputfp, Bit2_T bit2)
 {
@@ -294,12 +309,13 @@ void pbmWrite(FILE *outputfp, Bit2_T bit2)
     Bit2_map_row_major(bit2, printValues, NULL);
 }
 
-/* 
- * NAME:   printValues
- * DOES:   an apply function supplied to the Bit2_map_row_major function that
- *         prints the values in the bit2
- * INPUT:  the bit2, and the col (int) and row (int) to be printed
- * OUTPUT: returns nothing
+/*
+ *  printValues()
+ *
+ *  Parameters: Bit2_T, column and row to be printed
+ *  Does:       An apply function supplied to Bit2_map_row_major that prints
+ *              the values in the Bit2_T instance
+ *  Returns:    NA
  */
 void printValues(int col, int row, Bit2_T bit2, int b, void *p1)
 {
